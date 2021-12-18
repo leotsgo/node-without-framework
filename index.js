db060a37e5ce971e1801b7a94fdeebbe429b7312
leodiber <leonardodiber@gmail.com>
2021-12-18 17:51:28 -0300

    Cria um servidor que ouve na porta 3000

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Minha primeira API em node, sem utilizar framework algum
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.end('Hello World\n');
+});
+
+server.listen(3000, () => {
+    console.log('O servidor está rodando na porta 3000');
+    }); ;
