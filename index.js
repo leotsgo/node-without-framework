ebab1e58c0ef278b1cfd7b695cfcc8aa31341e2c
leodiber <leonardodiber@gmail.com>
2021-12-18 18:32:29 -0300

    Loga na tela o caminho, metodo e parametros da requisiçao

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 // Minha primeira API em node, sem utilizar framework algum
 
 const http = require('http');
+const url = require('url');
 
+// O servidor responderá a todas as requisições com uma string
 const server = http.createServer((req, res) => {
+    // Obtendo o caminho
+    const parsedUrl = url.parse(req.url, true); // transformando a url em objeto
+    const path = parsedUrl.pathname; // pegando o caminho
+    const trimmedPath = path.replace(/^\/+|\/+$/g, ''); // removendo os / do início e do fim
+
+    // Obtendo o método
+    const method = req.method.toLowerCase();
+
+    // Obtendo os parâmetros
+    const queryString = parsedUrl.query;
+
+    // Enviando a resposta
     res.end('Hello World\n');
+
+    // Logando a requisição
+    console.log(`Requisição recebida no caminho: ${trimmedPath} com o método ${method} e parâmetros ${queryString}`);
 });
 
 server.listen(3000, () => {
